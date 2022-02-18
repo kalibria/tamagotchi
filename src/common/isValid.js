f26e811d5d83a79393fcd207d72e5abf069f7ad0
Maria Kushlianskaya <mariakazakova92@gmail.com>
2022-02-18 15:28:26 +0300

    TAM-2. allow dash in name

--- src/common/isValid.js
+++ src/common/isValid.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const isValid = (text) => {
-    const regexp = new RegExp(/^[\sA-z]+$/);
+    const regexp = new RegExp(/^[\sA-z-]+$/);
 
     return regexp.test(text)
 }
